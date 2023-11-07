5557dfbf241df77c39b1441601b6e9492fcb9ed5
Michael Chop <007micle007@gmail.com>
2023-11-07 23:55:26 +0300

    Added sync file with some tests

---
+++ sync/sync.spec.js
@@ -0,0 +1,80 @@
+const Lodash = require('./sync')
+
+// TDD - test-driven development
+// сначала тесты, потом разработка под тесты
+
+let _ = new Lodash()
+
+describe('Lodash: compact', () => {
+    let array = []
+    let result = []
+
+    beforeEach(() => {
+        array = [false, 42, 0, '', true, null, 'text', undefined]
+        result = [42, true, 'text']
+    })
+
+    afterAll(() => {
+        _ = new Lodash()
+    })
+
+    test('should be defined', () => {
+        expect(_.compact).toBeDefined()
+        expect(_.compact).not.toBeUndefined()
+    })
+
+    test('should working array to be editable', () => {
+        array.push('one', 'two')
+        expect(array).toContain('one')
+        expect(array).toContain('two')
+    })
+
+    test('should remove falsy values from array', () => {
+        // toBe - для примитивов
+        // toEqual - для сложных структур
+
+        expect(_.compact(array)).toEqual(result)
+    })
+
+    test('should NOT contain falsy values', () => {
+        expect(_.compact(array)).not.toContain(false)
+        expect(_.compact(array)).not.toContain(0)
+        expect(_.compact(array)).not.toContain('')
+        expect(_.compact(array)).not.toContain(null)
+    })
+});
+
+describe('Lodash: groupBy', () => {
+    test('should be defined', () => {
+        expect(_.groupBy).toBeDefined()
+        expect(_.groupBy).not.toBeUndefined()
+    })
+
+    test('should group array items by Math.floor', () => {
+        const array = [2.2, 2.4, 4.2, 3.1]
+        const result = {
+            2: [2.2, 2.4],
+            4: [4.2],
+            3: [3.1]
+        }
+        expect(_.groupBy(array, Math.floor)).toEqual(result)
+    })
+
+    test('should group array items by length', () => {
+        const array = ['one', 'two', 'three']
+        const result = {
+            3: ['one', 'two'],
+            5: ['three']
+        }
+        expect(_.groupBy(array, 'length')).toEqual(result)
+    })
+
+    test('should NOT return array', () => {
+        const array = ['one', 'two', 'three']
+        const result = {
+            3: ['one', 'two'],
+            5: ['three']
+        }
+        expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array)
+    })
+})
